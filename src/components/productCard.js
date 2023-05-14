c34c20e22efdad3dcdf70d1762d1b7d8cd55350f
Mauritz Marais <mauritz303@gmail.com>
2023-05-14 11:49:39 +0200

    added add to cart button

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -1,11 +1,14 @@
 // card to display product information
+// import { keyboards, mice, } from "../inventory";
 
-function ProductCard() {
+function ProductCard({ product }) {
     return (
         <>
             <div>
-                <h3>Product name</h3>
-                <p>descrition ....</p>
+                <h3>{product.model}</h3>
+                <p>{product.brand}</p>
+                <p>{product.price}</p>
+                <button className="add-to-cart-btn">add to cart</button>
             </div>
         </>
     )
