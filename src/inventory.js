baeaa8a45e2a020d019ecf29fb4012b953223cc2
Mauritz Marais <mauritz303@gmail.com>
2023-05-14 11:48:42 +0200

    changed inventory to an object containing all categories

--- src/inventory.js
+++ src/inventory.js
@@ -1,24 +1,20 @@
 import { v4 as uuidv4 } from "uuid"
 
-// keyboards
-export const keyboards = [
-    { id: uuidv4(), brand: 'steelseries', model: 'k500', price: '100.00', img: '' }
-]
+export const inventory = {
+    keyboards: [
+        { id: uuidv4(), brand: 'steelseries', model: 'k500', price: '100.00', img: '' }
+    ],
+    mice: [
+        { id: uuidv4(), brand: 'razer', model: 'viper mini', price: '50.00', img: '' }
+    ],
+    headsets: [
+        { id: uuidv4(), brand: 'corsair', model: 'c700', price: '70.00', img: '' }
+    ],
+    controllers: [
+        { id: uuidv4(), brand: 'xbox', model: 'series', price: '80', img: '' }
+    ]
+
+}
 
 
-// mice
-export const mice = [
-    { id: uuidv4(), brand: 'razer', model: 'viper mini', price: '50.00', img: '' }
-]
 
-
-// headsets
-export const headsets = [
-    { id: uuidv4(), brand: 'corsair', model: 'c700', price: '70.00', img: '' }
-]
-
-
-// controllers
-export const controllers = [
-    { id: uuidv4(), brand: 'xbox', model: 'series', price: '80', img: '' }
-]
