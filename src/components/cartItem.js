55f4416d1362b2e78756cf7a7623ce5f8dab81ad
Mauritz Marais <mauritz303@gmail.com>
2023-05-15 16:40:58 +0200

    added component to display cart item

---
+++ src/components/cartItem.js
@@ -0,0 +1,14 @@
+
+function CartItem({ product, handleCartRemove }) {
+    return (
+        <>
+            <div>
+                <h4>{product.model}</h4>
+                <p>{product.price}</p>
+                <button onClick={() => handleCartRemove(product.instanceId)}>remove item</button>
+            </div>
+        </>
+    )
+}
+
+export default CartItem;
