79c69aa55289521e0f43477f94aabe4876bd01c9
Mauritz Marais <mauritz303@gmail.com>
2023-05-15 16:36:56 +0200

    updated shopping cart to conditionally render cart items and calculate total

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,1 +1,36 @@
-// shopping cart
+import CartItem from "./cartItem";
+import { Link } from "react-router-dom";
+
+function ShoppingCart({ cart, handleCartRemove }) {
+
+    if (!cart) {
+        return (
+            <>
+                <div className="shopping-cart">
+                    <h3>My cart</h3>
+                    <p>cart is empty</p>
+                    <Link to="/products">Continue shopping</Link>
+                </div>
+            </>
+        )
+    } else {
+        // calculate total
+        const total = cart.reduce((accumulator, currentValue) =>
+            accumulator + Number(currentValue.price), 0)
+        return (
+            <>
+                <div className="shopping-cart">
+                    <h3>My cart</h3>
+                    {cart.map((item, index) => {
+                        return <CartItem product={item} key={index} handleCartRemove={handleCartRemove} />
+                    })}
+                    <p>total: ${total}</p>
+                    <Link to="/products">Continue shopping</Link>
+                </div>
+            </>
+        )
+    }
+
+}
+
+export default ShoppingCart;
