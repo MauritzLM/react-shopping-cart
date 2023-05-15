29cbc9643a382722b4627bf0646d54505a04dc28
Mauritz Marais <mauritz303@gmail.com>
2023-05-15 16:35:36 +0200

    added cart state and add and remove functions

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/header';
@@ -5,16 +7,37 @@ import Home from './components/home';
 import Products from './components/productCategory';
 import ErrorPage from './components/error';
 import ProductCard from './components/productCard';
+import ShoppingCart from './components/cart';
+import { inventory } from './inventory';
+import { v4 as uuidv4 } from "uuid";
 
+// implement cart state*
 function App() {
+  const [cart, setCart] = useState([]);
+
+  // handle cart add
+  function handleCartAdd(category, itemid) {
+    // find item in category arr
+    const result = inventory[category].find(({ id }) => id === itemid);
+    result.instanceId = uuidv4();
+
+    setCart([...cart, result]);
+  }
+
+  // handle cart remove 
+  function handleCartRemove(instanceId) {
+    setCart(cart.filter(item => item.instanceId !== instanceId))
+  }
+
   return (
     <div className="App">
-      <Header />
+      <Header cart={cart} />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path='/shopping-cart' element={<ShoppingCart cart={cart} handleCartRemove={handleCartRemove} />} />
           <Route path="/products">
-            <Route index element={<Products />} />
+            <Route index element={<Products cart={cart} handleCartAdd={handleCartAdd} />} />
             <Route path=':id' element={<ProductCard />} />
           </Route>
           <Route path="*" element={<ErrorPage />} />
