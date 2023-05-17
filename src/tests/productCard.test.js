a24451a5b9494feb98e296d3d1b63e9a85c021e6
Mauritz Marais <mauritz303@gmail.com>
2023-05-17 16:26:29 +0200

    added test for add to cart button

--- src/tests/productCard.test.js
+++ src/tests/productCard.test.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 import ProductCard from "../components/productCard";
 import { v4 as uuidv4 } from "uuid"
 
 // object setup
 const product = { id: uuidv4(), brand: "corsair", model: "h500", price: "120", img: "" };
+
 
 describe("product card", () => {
     it("render card with correct information", () => {
@@ -14,5 +16,14 @@ describe("product card", () => {
         expect(screen.getByRole("button")).toHaveTextContent("add to cart");
     });
 
-    // test add to cart button*
+    it("clicking on add to cart button calls cart add function", () => {
+        const handleCartAdd = jest.fn();
+
+        render(<ProductCard product={product} handleCartAdd={handleCartAdd} />)
+
+        userEvent.click(screen.getByRole("button"));
+        userEvent.click(screen.getByRole("button"));
+
+        expect(handleCartAdd).toHaveBeenCalledTimes(2);
+    })
 });
