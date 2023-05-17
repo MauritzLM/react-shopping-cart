bad478ec7266f519aac23590f7d3e3dac7106fa2
Mauritz Marais <mauritz303@gmail.com>
2023-05-17 16:27:15 +0200

    added test for rendering of items in category

--- src/tests/productContainer.test.js
+++ src/tests/productContainer.test.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
 import ProductsContainer from "../components/productsContainer";
-import { inventory } from "../inventory";
+
+
+const handleCartAdd = jest.fn();
+const cart = [];
 
 // renders correct category
 describe("category browse", () => {
     it("renders correct category", () => {
         render(<ProductsContainer category={"controllers"} />)
         expect(screen.getByText("controllers")).toBeInTheDocument();
+        expect(screen.queryByText("k500")).not.toBeInTheDocument();
+    })
+
+    // renders all items in category
+    it("renders all items in category", () => {
+        render(<ProductsContainer category={"controllers"} cart={cart} handleCartAdd={handleCartAdd} />)
+
+        expect(screen.getByText("ps5")).toBeInTheDocument();
+        expect(screen.getByText("series")).toBeInTheDocument();
     })
 })
