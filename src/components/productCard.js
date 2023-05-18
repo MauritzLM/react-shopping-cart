2023253be8da7a8eb4ba7c8773c145986eecd786
Mauritz Marais <mauritz303@gmail.com>
2023-05-18 17:01:16 +0200

    added input to product card to select number of items and function to handle change of value

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -1,12 +1,22 @@
 
+import { useState } from "react";
+
 function ProductCard({ product, category, handleCartAdd }) {
+    const [numberOfItems, setNumberOfItems] = useState(1);
+
+    function handleItemsChange(event) {
+        setNumberOfItems(event.target.value);
+    }
+
     return (
         <>
             <div>
                 <h3>{product.model}</h3>
                 <p>{product.brand}</p>
                 <p>${product.price}</p>
-                <button className="add-to-cart-btn" onClick={e => handleCartAdd(category, product.id)}>add to cart</button>
+                <label htmlFor="item-amount">no.</label>
+                <input name="item-amount" type="number" min={1} step={1} onChange={(e) => handleItemsChange(e)} value={numberOfItems}></input>
+                <button className="add-to-cart-btn" onClick={e => handleCartAdd(category, product.id, numberOfItems)}>add to cart</button>
             </div>
         </>
     )
