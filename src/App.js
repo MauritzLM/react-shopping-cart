9e89d8111121606b14e4acb55957fe1410bd4006
Mauritz Marais <mauritz303@gmail.com>
2023-05-18 16:12:56 +0200

    removed total state, added function to reset cart to initial state

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ import Products from './components/productCategory';
 import ErrorPage from './components/error';
 import ProductCard from './components/productCard';
 import ShoppingCart from './components/cart';
+import Checkout from './components/checkout';
+import Confirmed from './components/purchaseConfirmed';
 import { inventory } from './inventory';
 import { v4 as uuidv4 } from "uuid";
 
+const emptyCart = [];
+
 function App() {
-  const [cart, setCart] = useState([]);
-  const [total, setTotal] = useState(0);
+  const [cart, setCart] = useState(emptyCart);
 
   // handle cart add
   function handleCartAdd(category, itemid) {
@@ -24,24 +27,17 @@ function App() {
     let cartItem = { ...result, instanceId: uuidv4() };
 
     setCart([...cart, cartItem]);
-
-    setTotal(cart.reduce((accumulator, currentValue) =>
-      accumulator + Number(currentValue.price), Number(result.price)))
   }
 
-  // handle cart remove* 
+  // handle cart remove
   function handleCartRemove(instanceId) {
     setCart(cart.filter(item => item.instanceId !== instanceId));
-
-    setTotal(cart.reduce((accumulator, currentValue) =>
-      accumulator + Number(currentValue.price), 0))
-
   }
 
-  // function calculateTotalPrice() {
-  //   setTotal(cart.reduce((accumulator, currentValue) =>
-  //     accumulator + Number(currentValue.price), 0))
-  // }
+  // handle confirmed purchase
+  function handleConfirmedPayment() {
+    setCart(emptyCart);
+  }
 
   return (
     <div className="App">
@@ -49,7 +45,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path='/shopping-cart' element={<ShoppingCart cart={cart} handleCartRemove={handleCartRemove} total={total} />} />
+          <Route path='/shopping-cart' element={<ShoppingCart cart={cart} handleCartRemove={handleCartRemove} handleConfirmedPayment={handleConfirmedPayment} />} />
           <Route path="/products">
             <Route index element={<Products cart={cart} handleCartAdd={handleCartAdd} />} />
             <Route path=':id' element={<ProductCard />} />
