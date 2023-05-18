8c16bbd27c417ab7a030e3290658a5c0787e1f99
Mauritz Marais <mauritz303@gmail.com>
2023-05-18 16:14:27 +0200

    added checkout state and conditional rendering for checkout state

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,8 +1,30 @@
 import CartItem from "./cartItem";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Checkout from "./checkout";
 
-function ShoppingCart({ cart, handleCartRemove, total }) {
+function ShoppingCart({ cart, handleCartRemove, handleConfirmedPayment }) {
+    const [checkout, setCheckout] = useState(false);
 
+    function handleCheckout() {
+        setCheckout(true);
+    }
+
+    function handleBackToCart() {
+        setCheckout(false);
+    }
+
+    // calculate total
+    const total = cart.reduce((accumulator, currentValue) =>
+        accumulator + Number(currentValue.price), 0)
+
+    if (checkout) {
+        return (
+            <>
+                <Checkout total={total} handleBackToCart={handleBackToCart} handleConfirmedPayment={handleConfirmedPayment} />
+            </>
+        )
+    }
     if (!cart.length) {
         return (
             <>
@@ -13,8 +35,8 @@ function ShoppingCart({ cart, handleCartRemove, total }) {
                 </div>
             </>
         )
-    } else {
-
+    }
+    else {
         return (
             <>
                 <div className="shopping-cart">
@@ -22,8 +44,9 @@ function ShoppingCart({ cart, handleCartRemove, total }) {
                     {cart.map((item, index) => {
                         return <CartItem product={item} key={index} handleCartRemove={handleCartRemove} />
                     })}
-                    <p>total: ${total}</p>
+                    <p>Total: ${total}</p>
                     <Link to="/products">Continue shopping</Link>
+                    <button onClick={() => handleCheckout()}>Proceed to checkout</button>
                 </div>
             </>
         )
