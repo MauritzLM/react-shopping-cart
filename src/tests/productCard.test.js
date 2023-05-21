8fb42f92296c4bcb6c5b2fd7ae4ea93c839cd211
Mauritz Marais <mauritz303@gmail.com>
2023-05-21 16:21:15 +0200

    implemented tests for changing of input value, and add to cart function

--- src/tests/productCard.test.js
+++ src/tests/productCard.test.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import ProductCard from "../components/productCard";
-import { v4 as uuidv4 } from "uuid"
 
 // object setup
-const product = { id: uuidv4(), brand: "corsair", model: "h500", price: "120", img: "" };
+const product = { id: "101", brand: "corsair", model: "h500", price: "120", img: "" };
 
 
 describe("product card", () => {
@@ -16,14 +15,30 @@ describe("product card", () => {
         expect(screen.getByRole("button")).toHaveTextContent("add to cart");
     });
 
-    it("clicking on add to cart button calls cart add function", () => {
-        const handleCartAdd = jest.fn();
+    it("input value changes with user input", () => {
+        // const user = userEvent.setup()
 
-        render(<ProductCard product={product} handleCartAdd={handleCartAdd} />)
+        render(<ProductCard product={product} />)
+
+        const input = screen.getByRole("spinbutton");
+
+        fireEvent.change(input, { target: { value: "4" } });
+
+        expect(input).toHaveValue(4);
+    });
+
+    it("calls add to cart function with correct arguments", () => {
+        const handleCartAddMock = jest.fn();
+
+        render(<ProductCard category={"keyboards"} product={product} handleCartAdd={handleCartAddMock} />)
+
+        const input = screen.getByRole("spinbutton");
+
+        fireEvent.change(input, { target: { value: "4" } });
 
         userEvent.click(screen.getByRole("button"));
-        userEvent.click(screen.getByRole("button"));
 
-        expect(handleCartAdd).toHaveBeenCalledTimes(2);
-    })
+        expect(handleCartAddMock).toHaveBeenCalledWith("keyboards", "101", "4");
+    });
+
 });
