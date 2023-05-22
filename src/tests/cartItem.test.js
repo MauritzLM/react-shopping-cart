e4814207ee5c3d73d261089d04fd98a3f3526f3a
Mauritz Marais <mauritz303@gmail.com>
2023-05-22 16:02:02 +0200

    cleaned up code

--- src/tests/cartItem.test.js
+++ src/tests/cartItem.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { screen, render, fireEvent } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import CartItem from "../components/cartItem";
@@ -9,7 +9,7 @@ const product = { id: "101", brand: "corsair", model: "h500", price: "120", inst
 
 describe("cart item", () => {
     it("cart item rendering", () => {
-        render(<CartItem product={product} />)
+        render(<CartItem product={product} />);
 
         expect(screen.getByRole("heading")).toHaveTextContent("h500");
         expect(screen.getByText("$120")).toBeInTheDocument();
@@ -18,10 +18,10 @@ describe("cart item", () => {
     it("cart remove function called with correct arguments", () => {
         const cartRemoveMock = jest.fn();
 
-        render(<CartItem product={product} handleCartRemove={cartRemoveMock} />)
+        render(<CartItem product={product} handleCartRemove={cartRemoveMock} />);
 
         userEvent.click(screen.getByRole("button"));
 
         expect(cartRemoveMock).toHaveBeenCalledWith("01");
     });
-})
+});
