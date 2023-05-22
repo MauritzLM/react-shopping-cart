62c43cd6ecccc03fc4dbbe55ec55e5aa5eaa05b3
Mauritz Marais <mauritz303@gmail.com>
2023-05-22 15:51:09 +0200

    added isLoading state to render while payment is loading

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -4,11 +4,27 @@ import Confirmed from "./purchaseConfirmed";
 
 function Checkout({ cart, total, handleBackToCart, handleConfirmedPayment }) {
     const [paymentMade, setPaymentMade] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
-    function handlePayment() {
-        setPaymentMade(true)
+    async function handlePayment() {
+
+        setIsLoading(true);
+
+        await new Promise((resolve) => {
+            setTimeout(() => resolve(setIsLoading(false)), 2000)
+        });
+
+        setPaymentMade(true);
 
         handleConfirmedPayment();
+    }
+
+    if (isLoading) {
+        return (
+            <>
+                <p>Loading...</p>
+            </>
+        )
     }
 
     if (paymentMade) {
