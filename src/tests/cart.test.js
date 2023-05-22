59e36856138d6243ebab66388c00fbb7ec2e9ab0
Mauritz Marais <mauritz303@gmail.com>
2023-05-22 16:00:54 +0200

    added tests for total calculation and cart items rendering

--- src/tests/cart.test.js
+++ src/tests/cart.test.js
@@ -4,25 +4,53 @@ import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import ShoppingCart from "../components/cart";
 
-// import memory router to wrap component - for link to work
+// import memory router to wrap component - for Link to work
 import { MemoryRouter } from "react-router-dom";
 
 // cart, handleCartRemove setup
-let cartRemoveMock = jest.fn();
+const cartRemoveMock = jest.fn();
+const cart = [{ id: "101", brand: "corsair", model: "h500", price: "120", img: "", instanceId: "01", },
+{ id: "102", brand: "xbox", model: "series", price: "80", img: "", instanceId: "02" }];
 
-// test correct rendering(when cart is empty and when not), test remove button* 
+const ShoppingCartTotal = ({ cart }) => {
+    // calculate total
+    const total = cart.reduce((accumulator, currentValue) =>
+        accumulator + Number(currentValue.price), 0)
+    return (
+        <>
+            <p data-testid="total">Total: ${total}</p>
+        </>
+    )
+};
+
+
 describe("shopping cart display", () => {
     it("shopping cart renders correctly when empty", () => {
 
-        render(<MemoryRouter><ShoppingCart cart={[]} handleCartRemove={cartRemoveMock} /></MemoryRouter>)
+        render(<MemoryRouter><ShoppingCart cart={[]} handleCartRemove={cartRemoveMock} /></MemoryRouter>);
 
         expect(screen.getByText("cart is empty")).toBeInTheDocument();
-    })
+    });
 
     it("remove button calls cart remove function", () => {
-        render(<MemoryRouter><ShoppingCart cart={[{ model: "ps5" }]} handleCartRemove={cartRemoveMock} /></MemoryRouter>);
-        userEvent.click(screen.getByRole("button"));
+        render(<MemoryRouter><ShoppingCart cart={cart} handleCartRemove={cartRemoveMock} /></MemoryRouter>);
+        const buttons = screen.getAllByRole("button");
+
+        userEvent.click(buttons[0]);
 
         expect(cartRemoveMock).toHaveBeenCalled();
-    })
-})
+    });
+
+    it("renders cart items", () => {
+        render(<MemoryRouter><ShoppingCart cart={cart} /></MemoryRouter>);
+
+        expect(screen.getByText("h500")).toBeInTheDocument();
+        expect(screen.getByText("series")).toBeInTheDocument();
+    });
+
+    it("calculates total and displays it correctly", () => {
+        render(<ShoppingCartTotal cart={cart} />);
+
+        expect(screen.getByTestId("total")).toHaveTextContent("Total: $200");
+    });
+});
