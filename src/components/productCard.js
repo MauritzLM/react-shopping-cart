3c34e5082723ea8f7a7dab6ed2046935f1e98361
Mauritz Marais <mauritz303@gmail.com>
2023-05-25 15:50:57 +0200

    added classes and image

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -10,10 +10,13 @@ function ProductCard({ product, category, handleCartAdd }) {
 
     return (
         <>
-            <div>
-                <h3>{product.model}</h3>
-                <p>{product.brand}</p>
-                <p>${product.price}</p>
+            <div className="product-card">
+                <div className="product-info">
+                    <h3>{product.model}</h3>
+                    <p>{product.brand}</p>
+                    <p>${product.price}</p>
+                    <div className="product-img"><img src={product.img} alt=""></img></div>
+                </div>
                 <label htmlFor="item-amount">no.</label>
                 <input name="item-amount" type="number" min={1} step={1} onChange={(e) => handleItemsChange(e)} value={numberOfItems}></input>
                 <button className="add-to-cart-btn" onClick={e => handleCartAdd(category, product.id, numberOfItems)}>add to cart</button>
