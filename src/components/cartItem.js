74c91c7f71001e7526141b207b51a7958a8af813
Mauritz Marais <mauritz303@gmail.com>
2023-05-26 17:38:53 +0200

    added product image

--- src/components/cartItem.js
+++ src/components/cartItem.js
@@ -2,8 +2,9 @@
 function CartItem({ product, handleCartRemove }) {
     return (
         <>
-            <div>
+            <div className="cart-item">
                 <h4>{product.model}</h4>
+                <div><img src={product.img} alt=""></img></div>
                 <p>${product.price}</p>
                 <button onClick={() => handleCartRemove(product.instanceId)}>remove item</button>
             </div>
