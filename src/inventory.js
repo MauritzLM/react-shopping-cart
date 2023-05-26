9d3575db1d7373b5c2c080860ffbd39e90c4cdda
Mauritz Marais <mauritz303@gmail.com>
2023-05-26 17:35:48 +0200

    updated inventory items

--- src/inventory.js
+++ src/inventory.js
@@ -9,7 +9,7 @@ import keyboard4 from "./images/keyboard5.jpeg";
 import mouse1 from "./images/mouse9.png";
 import mouse2 from "./images/mouse8.png";
 import mouse3 from "./images/mouse6.png";
-import mouse4 from "./images/mouse7.png";
+import mouse4 from "./images/mouse4.png";
 
 // headset images
 import headset1 from "./images/headset1.jpeg";
@@ -26,28 +26,28 @@ import controller4 from "./images/xbox2.jpeg";
 
 export const inventory = {
     keyboards: [
-        { id: uuidv4(), brand: 'steelseries', model: 'apex7', price: '100.00', img: keyboard1, instanceId: '' },
-        { id: uuidv4(), brand: 'razer', model: 'Sidewinder', price: '100.00', img: keyboard2, instanceId: '' },
-        { id: uuidv4(), brand: 'logitech', model: 'k500', price: '100.00', img: keyboard3, instanceId: '' },
-        { id: uuidv4(), brand: 'logitech', model: 'k500', price: '100.00', img: keyboard4, instanceId: '' }
+        { id: uuidv4(), brand: 'Asus', model: 'TUF Gaming', price: '95.99', img: keyboard1, instanceId: '' },
+        { id: uuidv4(), brand: 'Cooler Master', model: 'CK721', price: '105.89', img: keyboard2, instanceId: '' },
+        { id: uuidv4(), brand: 'Logitech', model: 'TKL SE', price: '110.89', img: keyboard3, instanceId: '' },
+        { id: uuidv4(), brand: 'Keychron', model: 'Tenkeyless', price: '149.99', img: keyboard4, instanceId: '' }
     ],
     mice: [
-        { id: uuidv4(), brand: 'razer', model: 'viper mini', price: '50.00', img: mouse1, instanceId: '' },
-        { id: uuidv4(), brand: 'razer', model: 'viper mini', price: '50.00', img: mouse2, instanceId: '' },
-        { id: uuidv4(), brand: 'razer', model: 'viper mini', price: '50.00', img: mouse3, instanceId: '' },
-        { id: uuidv4(), brand: 'razer', model: 'viper mini', price: '50.00', img: mouse4, instanceId: '' }
+        { id: uuidv4(), brand: 'Cougar', model: 'Minos X5', price: '69.99', img: mouse1, instanceId: '' },
+        { id: uuidv4(), brand: 'Corsair', model: 'Scimitar', price: '149.99', img: mouse2, instanceId: '' },
+        { id: uuidv4(), brand: 'Logitech', model: 'G502', price: '139.99', img: mouse3, instanceId: '' },
+        { id: uuidv4(), brand: 'Gigabyte', model: 'Aorus M4', price: '59.89', img: mouse4, instanceId: '' }
     ],
     headsets: [
-        { id: uuidv4(), brand: 'corsair', model: 'c700', price: '70.00', img: headset1, instanceId: '' },
-        { id: uuidv4(), brand: 'corsair', model: 'c700', price: '70.00', img: headset2, instanceId: '' },
-        { id: uuidv4(), brand: 'corsair', model: 'c700', price: '70.00', img: headset3, instanceId: '' },
-        { id: uuidv4(), brand: 'corsair', model: 'c700', price: '70.00', img: headset4, instanceId: '' }
+        { id: uuidv4(), brand: 'Redragon', model: 'H510', price: '79.99', img: headset1, instanceId: '' },
+        { id: uuidv4(), brand: 'HyperX', model: 'Cloud Wireless', price: '89.99', img: headset2, instanceId: '' },
+        { id: uuidv4(), brand: 'Redragon', model: 'Lamia 2', price: '75.99', img: headset3, instanceId: '' },
+        { id: uuidv4(), brand: 'Steelseries', model: 'Arctis 7', price: '200.99', img: headset4, instanceId: '' }
     ],
     controllers: [
-        { id: uuidv4(), brand: 'xbox', model: 'series', price: '80', img: controller1, instanceId: '' },
-        { id: uuidv4(), brand: 'sony', model: 'ps5', price: '85', img: controller2, instanceId: '' },
-        { id: uuidv4(), brand: 'sony', model: 'ps5', price: '85', img: controller3, instanceId: '' },
-        { id: uuidv4(), brand: 'sony', model: 'ps5', price: '85', img: controller4, instanceId: '' }
+        { id: uuidv4(), brand: 'Playstation', model: 'ps4 Black', price: '59.99', img: controller1, instanceId: '' },
+        { id: uuidv4(), brand: 'Playstation', model: 'ps4 Camo', price: '65.99', img: controller2, instanceId: '' },
+        { id: uuidv4(), brand: 'Xbox', model: 'Series White', price: '85.99', img: controller3, instanceId: '' },
+        { id: uuidv4(), brand: 'Xbox', model: 'Series Black', price: '85.99', img: controller4, instanceId: '' }
     ]
 
 }
