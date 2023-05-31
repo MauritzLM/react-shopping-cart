22c8568674c9f2bb6f534d79b0877ee259eb148c
Mauritz Marais <mauritz303@gmail.com>
2023-05-31 13:19:14 +0200

    added memory router to wrap component

--- src/tests/productCategory.test.js
+++ src/tests/productCategory.test.js
@@ -3,16 +3,17 @@ import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import Products from "../components/productCategory";
+import { MemoryRouter } from "react-router-dom";
 
 // Select element
-describe("browse products page", () => {
+describe("browse products page rendering", () => {
     it("renders select correctly", () => {
-        render(<Products />)
+        render(<MemoryRouter><Products /></MemoryRouter>)
         expect(screen.getByRole("combobox")).toHaveLength(4);
     })
 
     it("changes value with user select", () => {
-        render(<Products />)
+        render(<MemoryRouter><Products /></MemoryRouter>)
         userEvent.selectOptions(screen.getByRole("combobox"), "headsets")
 
         expect(screen.getByRole("combobox")).toHaveValue("headsets");
