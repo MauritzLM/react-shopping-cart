b3b3828148cbaee4ede2002acbc532266aa71aab
Mauritz Marais <mauritz303@gmail.com>
2023-05-31 13:20:50 +0200

    removed unnecessary expect, completed final test

--- src/tests/productContainer.test.js
+++ src/tests/productContainer.test.js
@@ -3,23 +3,23 @@ import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ProductsContainer from "../components/productsContainer";
 
-
 const handleCartAdd = jest.fn();
 const cart = [];
 
 // renders correct category
 describe("category browse", () => {
     it("renders correct category", () => {
-        render(<ProductsContainer category={"controllers"} />)
-        expect(screen.getByText("controllers")).toBeInTheDocument();
-        expect(screen.queryByText("k500")).not.toBeInTheDocument();
-    })
+        render(<ProductsContainer category={"keyboards"} />)
+        expect(screen.queryByText("ps4 Camo")).not.toBeInTheDocument();
+    });
 
     // renders all items in category
     it("renders all items in category", () => {
-        render(<ProductsContainer category={"controllers"} cart={cart} handleCartAdd={handleCartAdd} />)
+        render(<ProductsContainer category={"keyboards"} cart={cart} handleCartAdd={handleCartAdd} />)
 
-        expect(screen.getByText("ps5")).toBeInTheDocument();
-        expect(screen.getByText("series")).toBeInTheDocument();
-    })
-})
+        expect(screen.getByText("Tenkeyless")).toBeInTheDocument();
+        expect(screen.getByText("TUF Gaming")).toBeInTheDocument();
+        expect(screen.getByText("CK721")).toBeInTheDocument();
+        expect(screen.getByText("TKL SE")).toBeInTheDocument();
+    });
+});
